98c4e6c1dfdc8980a1bf3c0b9303b09c2196b409
BT_C09_33 <mobileappdev@bths.edu>
2026-04-22 12:33:33 -0400

    Initial Commit

---
+++ collaborationfun/collaborationfunApp.swift
@@ -0,0 +1,17 @@
+//
+//  collaborationfunApp.swift
+//  collaborationfun
+//
+//  Created by Teacher on 4/22/26.
+//
+
+import SwiftUI
+
+@main
+struct collaborationfunApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
