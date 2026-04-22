43b2486456f4a0ca19bc106f522c6228a0b8b7ea
BT_C09_33 <mobileappdev@bths.edu>
2026-04-22 16:49:34 -0400

    added Image

--- collaborationfun/ContentView.swift
+++ collaborationfun/ContentView.swift
@@ -15,6 +15,7 @@ struct ContentView: View {
                 .foregroundStyle(.tint)
             Text("hello world!!")
             Text("hiiii")
+            Image(systemName: "globe")
         }
         .padding()
     }
