0dac07afa7255aa7ed57d20e48d4690e04271ba2
BT_C09_01 <mobileappdev@bths.edu>
2026-04-22 16:48:44 -0400

    added Text

--- collaborationfun/ContentView.swift
+++ collaborationfun/ContentView.swift
@@ -14,6 +14,7 @@ struct ContentView: View {
                 .imageScale(.large)
                 .foregroundStyle(.tint)
             Text("hello world!!")
+            Text("hiiii")
         }
         .padding()
     }
